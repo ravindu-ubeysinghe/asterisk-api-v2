71b97aa940149c9aba71372063f7574f7b118357
ravindu.ubeysinghe <ravindu.ubeysinghe@beteasy.com.au>
2020-05-08 12:50:54 +1000

    feat: added get all users function

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,19 @@
 import express, { Router, Request, Response } from 'express';
-import { createUser } from 'services/user.service';
+import { createUser, getUsers } from 'services/user.service';
 import { CreateUserRequest } from 'models/user/user.dataContracts';
-import { RECORD_CREATION_FAILED } from 'constants/index';
 import response from 'utils/response';
 
 const router: Router = express.Router();
+
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        const users = await getUsers();
+        response.success(res, 200, users);
+    } catch (err) {
+        // TODO: Add proper logging after
+        response.error(res, 500, err.message);
+    }
+});
 
 router.post('/register', async (req: Request, res: Response) => {
     const userData: CreateUserRequest = {
@@ -22,13 +31,12 @@ router.post('/register', async (req: Request, res: Response) => {
         },
     };
 
-    // Add validator middleware
+    // TODO: Add validator middleware
 
     try {
         const createdUser = await createUser(userData);
         response.success(res, 200, createdUser);
     } catch (err) {
-        // TODO: Add proper logging after
         response.error(res, 500, err.message);
     }
 });
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,15 @@
 import User from 'models/user/user.model';
 import { CreateUserRequest } from 'models/user/user.dataContracts';
 
+export const getUsers = async () => {
+    try {
+        return await User.find({});
+    } catch (err) {
+        console.log(err);
+        throw new Error(err.message);
+    }
+};
+
 export const createUser = async (data: CreateUserRequest) => {
     const user = new User({ ...data });
     try {
